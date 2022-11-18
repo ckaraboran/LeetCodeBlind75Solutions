cc9ad76e2723290f36adf4f644bf9710819c9797
Can Karaboran <can.karaboran@carnext.com>
2022-11-18 22:27:10 +0100

    feat: add two sum calculator

---
+++ TwoSum/OnePassHash.cs
@@ -0,0 +1,18 @@
+namespace TwoSum;
+
+public class OnePassHash : ITwoSumCalculator
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (map.ContainsKey(complement)) return new[] { map[complement], i };
+            map.Add(nums[i], i);
+        }
+
+        // In case there is no solution, we'll just return null
+        return Array.Empty<int>();
+    }
+}
---
+++ TwoSum/TwoPassHash.cs
@@ -0,0 +1,18 @@
+namespace TwoSum;
+
+public class TwoPassHash : ITwoSumCalculator
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var map = new Dictionary<int, int>();
+        for (var i = 0; i < nums.Length; i++) map.Add(nums[i], i);
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var complement = target - nums[i];
+            if (map.ContainsKey(complement) && map[complement] != i) return new[] { i, map[complement] };
+        }
+
+        // In case there is no solution, we'll just return null
+        return Array.Empty<int>();
+    }
+}
