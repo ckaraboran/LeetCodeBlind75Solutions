d97eb8d6582f76acc7a5a3bdb3bd168f90d61e89
Can Karaboran <can.karaboran@carnext.com>
2022-11-24 22:47:25 +0100

    feat: add maximum product subarray solution

---
+++ MaximumProductSubarray/OnePass.cs
@@ -0,0 +1,28 @@
+namespace MaximumProductSubarray;
+
+public class OnePass : IMaximumProductSubarrayFinder
+{
+    public int MaxProduct(int[] nums)
+    {
+        int max = nums[0], min = nums[0], ans = nums[0];
+        int n = nums.Length;
+
+        for (int i = 1; i < n; i++)
+        {
+            // Swapping min and max
+            if (nums[i] < 0)
+            {
+                (max, min) = (min, max);
+            }
+
+
+            max = Math.Max(nums[i], max * nums[i]);
+            min = Math.Min(nums[i], min * nums[i]);
+
+
+            ans = Math.Max(ans, max);
+        }
+
+        return ans;
+    }
+}
---
+++ MaximumProductSubarray/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Given an integer array nums, find a subarray that has the largest product, and return the product.
+ The test cases are generated so that the answer will fit in a 32-bit integer.
+ */
+
+using MaximumProductSubarray;
+
+var innerCalculators = new List<IMaximumProductSubarrayFinder>();
+innerCalculators.Add(new OnePass());
+
+Calculate(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, innerCalculators);
+Calculate(new[] { 1, -2, 4, 3, -1, 5, 3 }, innerCalculators);
+Calculate(new[] { 2, 3, -2, 4 }, innerCalculators);
+Calculate(new[] { -2, 0, -1 }, innerCalculators);
+
+
+void Calculate(int[] values, List<IMaximumProductSubarrayFinder> calculators)
+{
+    foreach (var calculator in calculators)
+    {
+        var result = calculator.MaxProduct(values);
+        Console.WriteLine($"{calculator.ToString()}: {result}");
+    }
+}
